5ab271a1c537657b0cbc6d791b2105e737e0b912
ShootKing233 <shootking233@outlook.com>
2021-04-24 18:50:06 +0800

    更新ChatServer类(still wip

--- core/server.py
+++ core/server.py
@@ -1,9 +1,29 @@
-# from flask import Flask
+from flask import Flask, Response
 from utils.singleton import Singleton
 
 
-@Singleton
-class ChatServer:
+class ServerAction(object):
 
-    def __init__(self):
-        pass
+    def __init__(self, action):
+        self.action = action
+
+    def __call__(self):
+        try:
+            answer = self.action()
+            self.response = Response(answer, status=200, headers={})
+        except Exception as e:
+            self.response = Response(e, status=500, headers={})
+        return self.response
+
+
+@Singleton
+class ChatServer(object):
+
+    def __init__(self, flask_: Flask):
+        self.flask_ = flask_
+
+    def run(self, *args):
+        self.flask_.run(*args)
+
+    def add_route(self, route=None, route_name=None, handler=None):
+        self.flask_.add_url_rule(route, route_name, ServerAction(handler))
